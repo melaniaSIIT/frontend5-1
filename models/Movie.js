4c9eeb2727f8ad0888eb4be3208b681e2516a7d4
Victor <banaru@gmail.com>
2018-09-04 12:04:06 +0300

    Added code to View and Models for the Home page, work in progress

--- models/Movie.js
+++ models/Movie.js
@@ -5,35 +5,22 @@ function Movie(options) {
     this.genre = options.Genre;
     this.posterUrl = options.Poster;
     this.rating = options.imdbRating;
-    this.votes = options.imdgVotes;
-    this.description = options.description;
+    this.votes = options.imdbVotes;
+    this.description = options.Plot;
   }
   
   Movie.prototype.getMovieDetails = function() {
     var that = this;
     return $.get("https://ancient-caverns-16784.herokuapp.com/movies" + this.id)
       .then(function(response) {
-        that.id = options._id;
-        that.title = options.Title;
-        that.genre = options.Genre;
-        that.posterUrl = options.Poster;
-        that.rating = options.imdbRating;
-        that.votes = options.imdgVotes;
-        that.description = options.description; 
+        that.id = response._id;
+        that.title = response.Title;
+        that.genre = response.Genre;
+        that.posterUrl = response.Poster;
+        that.rating = response.imdbRating;
+        that.votes = response.imdbVotes;
+        that.plot = response.Plot;
     })
   }
-  
-  Movie.prototype.editMovieDetails = function() {
-    return $.ajax({
-        url:  "https://ancient-caverns-16784.herokuapp.com/movies" + movieId,
-        method: "PUT",
-        data: {
-            // to be filled with data input from user
-        }, 
-        success: function() {
-
-        }
-      })
-      }
   
   
--- views/home.js
+++ views/home.js
@@ -1,24 +1,25 @@
 window.addEventListener("load", function() {
-    var containerElement = document.getElementById("movies-list");
+    let containerElement = document.getElementById("movies-list");
     
-    var moviesModel = new Movies();
+    let moviesModel = new Movies();
     moviesModel.getAll().then(function(response) {
       displayAllMovies(response);
       console.log(response);
     });
     
     function displayAllMovies(moviesData) {
-      for (var i = 0; i < moviesData.length; i++) {
-        var movie = new Movie(moviesData[i]);
+      for (let i = 0; i <= 10; i++) {
+        let movie = new Movie(moviesData[i]);
         displayMovie(movie);
+        console.log("Movie data", movie);
       }
     }
     
     function displayMovie(movie) {
-      var liEl = document.createElement('li');
+      let liEl = document.createElement('li');
       liEl.style = "list-style-type: none;";
       
-      var titleEl = document.createElement('h1');
+      let titleEl = document.createElement('h1');
       titleEl.innerHTML = movie.title;
       
       titleEl.addEventListener("mouseover", function () {
@@ -26,54 +27,36 @@ window.addEventListener("load", function() {
       });
           
       titleEl.addEventListener("click", function() {
-        window.location = "http://cursuri-v-banaru-banaru702140.codeanyapp.com/Homeworks/Homework%20-%20Objects%20and%20Classes/templates/movie.html?movieId=" + movie.id;
+        window.location = "https://ancient-caverns-16784.herokuapp.com/movies/" + movie.id;
       });
       
-      var imgEl = document.createElement('img');
+      let imgEl = document.createElement('img');
         $(imgEl).attr({
-          "src":movie.imageUrl,
+          "src":movie.posterUrl,
           "height":"200",
           "alt":"Movie Image here",
-        });
-  //       imgEl.setAttribute("height", "200");
-  //       imgEl.setAttribute("alt", "Movie Image here");
+        });  
         imgEl.style.display = "block";
       
-      var bodyEl = document.createElement('p');
+      let bodyEl = document.createElement('p');
       bodyEl.innerHTML = movie.description;
       
-      var idEl = document.createElement('p');
+      let idEl = document.createElement('p');
       idEl.innerHTML = movie.id;
       
-      var editBtn = document.createElement('button');
+      let editBtn = document.createElement('button');
       editBtn.innerHTML = "   Edit   ";
       editBtn.setAttribute("id", "edit-" + movie.id);
       editBtn.setAttribute("name", "Edit");
       editBtn.style.margin = "10px";
   
-      var deleteBtn = document.createElement('button');
+      let deleteBtn = document.createElement('button');
       deleteBtn.innerHTML = "  Delete  ";
       deleteBtn.setAttribute("id", "delete-" + movie.id);
       deleteBtn.setAttribute("name", "Delete");
       deleteBtn.style.margin = "10px";
-      
-      var movieId = movie.id;
-      
-  //     console.log("Movie's link to API - in order to be deleted", "https://movies-world.herokuapp.com/movies/" + movieId);
-  //     var deleteMovie = function(movieId) {
-  //       $.ajax({
-  //       url: "https://movies-world.herokuapp.com/movies/" + movieId,
-  //       method: "DELETE",
-  //       success: function() {
-  //         console.log("Movie was deleted");
-  //       }
-  //     })
-  //     }
-      
-      editBtn.addEventListener("click", editMovieDetails.movie());
-  //     deleteBtn.addEventListener("click", deleteMovie(movieId));
-  
-      
+           
+        
       liEl.appendChild(titleEl);
       liEl.appendChild(bodyEl);
       liEl.appendChild(imgEl);
