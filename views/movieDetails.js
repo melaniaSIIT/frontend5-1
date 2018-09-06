cb931611342b3b3887313a381c9eeb42e7cdd182
karina <crisan.karina31@yahoo.com>
2018-09-06 16:53:34 +0300

    movie details page

--- views/movieDetails.js
+++ views/movieDetails.js
@@ -9,24 +9,60 @@ window.addEventListener("load", function() {
       displayMovieDetails(movie);
       function displayMovieDetails(movieDetails) {
         
-        var titleEl = document.createElement('h1');
+        var titleEl = document.createElement('h2');
         titleEl.innerHTML = movieDetails.title;
+        titleEl.setAttribute("id", "movieTitleId");
         containerEl.appendChild(titleEl);
         
         var imageEl = document.createElement('IMG');
           imageEl.setAttribute("src", movieDetails.posterUrl);
           imageEl.setAttribute("height", "200");
           imageEl.setAttribute("alt", "Movie Image here");
+          imageEl.setAttribute("id", "movieImageId");
         containerEl.appendChild(imageEl);
           
         var bodyEl = document.createElement('p');
         bodyEl.innerHTML = movieDetails.plot;
+        bodyEl.setAttribute("id", "moviePlotId");
         containerEl.appendChild(bodyEl);
         
+        //buttons
+        var actionContainer = document.createElement("div");
+
+        var backButton = document.createElement("button");
+        backButton.innerHTML = "Back";
+        backButton.addEventListener("click", function(){
+          window.location = "../pages/home.html#home";
+        })
+        backButton.setAttribute("class", "actionButtonClass");
+        
+        var editButton = document.createElement("button");
+        editButton.innerHTML = "Edit";
+        editButton.setAttribute("class", "actionButtonClass");
+        
+        var deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "Delete";
+        deleteButton.setAttribute("class", "actionButtonClass");
+
+        actionContainer.appendChild(backButton);
+        actionContainer.appendChild(editButton);
+        actionContainer.appendChild(deleteButton);
+
+        containerEl.appendChild(actionContainer);
+
+        //Rate movie
+        var asideRateContainer = document.getElementById("asideSection");
+
+        //Rating Text
+        var rateMovieText = document.createElement("h2");
+        rateMovieText.innerHTML = "Rate " +  movieDetails.title;
+        asideRateContainer.appendChild(rateMovieText);
+        
+        // //Rating Alert
       }
     });
-    
-    
+
+
     /**
        * It retrieves a query (URL) parameter value
        * 
@@ -39,3 +75,5 @@ window.addEventListener("load", function() {
           return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
       }
   });
+
+
