98f34bc009303ca1097999045159b539868b43ea
Adrian Petrisor <adrian.petrisor@outlook.com>
2018-09-24 14:10:40 +0300

    fix display

--- views/home.js
+++ views/home.js
@@ -1,29 +1,11 @@
 window.addEventListener("load", function() {
-  
-    let isSearching = false;
-    
-    function checkIfSearchIsPresent() {
-      const search = location.search.substr(1);
-      const attribute = search.split('=')[0];
-      const searchValue = search.split('=')[1];
-
-      if (attribute === 'search') {
-        isSearching = true
-        $('#search').val(searchValue);
-        searchMovies(searchValue);
-      }
-    }
-
-
-    checkIfSearchIsPresent()
 
 
   let accessTokenCookieValue = Cookies.get("accessToken");
 
-
     // function that hides spinner must be called after games are loaded on screen, at the end of display all movies function
     function hideSpinner(){
-      $('.loader').css('display','none');
+    $('.loader').css('display','none');
     }
 
     // get container for all movies
@@ -63,34 +45,17 @@ window.addEventListener("load", function() {
     
   }
 
-	function displayAllMovies(movies) {
-	  containerElement.innerHTML = "";
-	  const moviesData = movies.results;
+  function displayAllMovies(movies) {
+    containerElement.innerHTML = "";
+    const moviesData = movies.results;
       for (let i = 0; i < moviesData.length; i++) {
         let movie = new Movie(moviesData[i]);
-        displayMovie(movie);	
+        displayMovie(movie);  
       }
       hideSpinner();
     }
 
-
-    currentUrl = $(location).attr("href");
-    let pageRef = currentUrl.substr(currentUrl.length - 7);
-    let skipPages = $("a[href='" + pageRef + "']").data("value");
-    if (typeof skipPages == "undefined"){
-      skipPages = 0;
-    }
-
-    skipValue = skipPages;
-
-    $('a', '.pagination').click(function(){
-      location.reload();
-    });
-
-    if (isSearching === false) {
-      moviesModel.getAll(takeValue, skipValue).then(displayAllMovies);
-    }
-
+   
 
     moviesModel.getAll(PAGE_ITEMS, skipValue).then((movies) => {
       displayPagination(movies);
@@ -123,42 +88,27 @@ window.addEventListener("load", function() {
       deleteBtn.setAttribute("id", "delete-" + movie.id);
       deleteBtn.setAttribute("name", "Delete");
       deleteBtn.setAttribute("class", "admin-button delete-button");
-
-	    deleteBtn.addEventListener("click", function() {
-        movie.deleteMovie(accessTokenCookieValue).then(function() {
-          alert(movie.title + "has been deleted!");
-          moviesModel.getAll().then(displayAllMovies);
-        });
-	    });
-
-      // moreInfoButton
-      let moreInfoButton = document.createElement('button');
-      moreInfoButton.innerHTML = "More Info";
-      moreInfoButton.setAttribute("class", "admin-button");
-      moreInfoButton.style.display = "inline-block";
-
-	  deleteBtn.addEventListener("click", function() {
-		movie.deleteMovie(accessTokenCookieValue).then(function() {
+    deleteBtn.addEventListener("click", function() {
+    movie.deleteMovie(accessTokenCookieValue).then(function() {
       alert(movie.title + "has been deleted!");
       location.reload();
-		});
-	  });
+    });
+    });
 
     // moreInfoButton
     let moreInfoButton = document.createElement('button');
     moreInfoButton.innerHTML = "More Info";
     moreInfoButton.setAttribute("class", "admin-button");
     moreInfoButton.style.display = "inline-block";
-
     
-      moreInfoButton.addEventListener("click", function(){
-        window.location = "../pages/movieDetails.html?movieId=" + movie.id;
-      });
+    moreInfoButton.addEventListener("click", function(){
+      window.location = "../pages/movieDetails.html?movieId=" + movie.id;
+    });
 
-      let adminButtons = document.createElement('div');
-      adminButtons.appendChild(editBtn);
-      adminButtons.appendChild(deleteBtn);
-      adminButtons.appendChild(moreInfoButton);
+    let adminButtons = document.createElement('div');
+    adminButtons.appendChild(editBtn);
+    adminButtons.appendChild(deleteBtn);
+    adminButtons.appendChild(moreInfoButton);
 
       let item = document.createElement('div');
       
@@ -171,11 +121,11 @@ window.addEventListener("load", function() {
       });
       
       let imgEl = document.createElement('img');
-      $(imgEl).attr({
-        "src":movie.posterUrl,
-        "height":"200",
-        "alt":"Movie Image here",
-      });  
+        $(imgEl).attr({
+          "src":movie.posterUrl,
+          "height":"200",
+          "alt":"Movie Image here",
+        });  
       
       let bodyEl = document.createElement('p');
       bodyEl.innerHTML = "Genre: " + movie.genre;
@@ -185,11 +135,14 @@ window.addEventListener("load", function() {
 
       let ratingEl = document.createElement('p');
       ratingEl.innerHTML = "IMDB Rating: " + movie.rating;
+
       
       let idEl = document.createElement('p');
       idEl.innerHTML = movie.id;
       
       // item.appendChild(moreInfoButton);
+      
+
         
       item.appendChild(titleEl);
       item.appendChild(bodyEl);
@@ -202,25 +155,22 @@ window.addEventListener("load", function() {
      
       containerElement.appendChild(item);
       showAdminButtons();
+
+
     }
   
     // SEARCH MOVIE by title
 
     $('#searchButton').on('click', function() {
-      let searchFor = $('#search').val();
-      searchMovies(searchFor)
-    });
-
-    function searchMovies(searchFor) {
-      isSearching = true
+      let searchFor = $('#search').val(); 
       let newMovies = new Movies();
       newMovies.searchByTitle(searchFor).then(function(response) {
         containerElement.innerHTML = '';
         displayAllMovies(response);
-        isSearching = false;
-        history.pushState({}, document.title, "home.html");      
+        console.log(response.results);
       });
-    }
+    });
+
 
     var input = document.getElementById("search");
       input.addEventListener("keyup", function(event) {
@@ -234,10 +184,5 @@ window.addEventListener("load", function() {
     $('#add-movies').on('click', function() {
       window.open("../pages/addMovie.html","_self");
     });
+
 })
-
-
-
-
-
-
